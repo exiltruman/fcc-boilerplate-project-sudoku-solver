735b8e54418d0456950baeb0668a71f64c583e3a
Maros Karasek <maros.karasek@gmail.com>
2025-03-18 13:43:24 +0000

    changes

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -10,11 +10,18 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    const rowValues = puzzleString.slice(row*9, 9);
+    return (rowValues.indexOf(value) === -1)
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    const columnValues = puzzleString.split("").filter((char, index) => {
+      if((index-(column))%9 === 0) return char;
+    }).join("")
 
+    console.log(columnValues);
+
+    return (columnValues.indexOf(value) === -1)
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,16 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
+const ps = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'
 
 suite('Unit Tests', () => {
-
+    test('Logic handles a valid row placement', () => {
+        const result = solver.checkRowPlacement(ps, 0, 0, 3);
+        assert.isTrue(result);
+    })
+    test('Logic handles a valid column placement', () => {
+        const result = solver.checkColPlacement(ps, 0, 8, 1);
+        assert.isTrue(result);
+    })
 });
